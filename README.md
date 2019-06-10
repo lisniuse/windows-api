ef84f55701e7ec230eecf807a5485b2b2e6b6686
lisniuse <17560235@qq.com>
2019-06-11 00:37:39 +0800

    feat: add EnumWindows

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# windows-api
-c++ 调用 windows-api 练习
+## C++ windows 平台 api 调用实现
+
+> C++ windows 平台 api 调用实现；开发环境：Windows + Vscode + LLVM + mingw64。
+
+- [枚举窗口(EnumWindows)](/src/EnumWindows.cpp)
---
+++ src/EnumWindows.cpp
@@ -0,0 +1,25 @@
+#include "windows.h"
+#include "iostream"
+
+using namespace std;
+
+CHAR windowText[MAXBYTE], className[MAXBYTE];
+WORD windowNumber = 0;
+
+BOOL CALLBACK EnumWindowsProc(
+	HWND hwnd,
+	LPARAM lParam
+) {
+  windowNumber++;
+	GetWindowText(hwnd, windowText, 200);
+  GetClassName(hwnd, className, 200);
+  
+  cout << windowNumber << " [Window Name] " << (strlen(windowText) ? windowText : "null") << " [Class Name] " << className << endl;
+	return TRUE;
+}
+
+int main() {
+  EnumWindows(EnumWindowsProc, NULL);
+  while(TRUE);
+	return 0;
+}
